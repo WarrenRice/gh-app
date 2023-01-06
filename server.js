3d6f2861e3c08e2296907a6629ecd8b001b60e63
Warin Poomarin <warren.rice.ca@gmail.com>
2023-01-06 14:05:23 -0400

    edit port in server

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = express();
 //require('dotenv').config();
 
 const api_key = process.env.API_KEY;
-app.listen(3000, () => console.log('listening to 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening to ${port}`));
 app.use(express.static('public'));
